e8f9da067358369cfdb74cd4db086828aca61e42
vigneshsaurav <vigneshsaurav052@gmail.com>
2026-03-20 23:23:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (87.84%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean searchMatrix(int[][] m, int t) {
+        int r=0, c=m[0].length-1;
+        while(r<m.length && c>=0){
+            if(m[r][c]==t) return true;
+            else if(m[r][c]>t) c--;
+            else r++;
+        }
+        return false;
+    }
+}
